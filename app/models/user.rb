879608ea0f63e50a4cdcbb815a0179b669885fdf
yuka52 <clrwtr.710@gmail.com>
2021-03-03 15:19:51 +0900

    新規登録機能を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,18 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true 
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/} do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  
 end
