44592f6c2359bbd08bab0319d17f850529e96cd3
yuka52 <clrwtr.710@gmail.com>
2021-03-03 18:20:47 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,24 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true 
+  with_options presence: true do       
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters'} 
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters'} 
+    validates :last_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters'}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters'}
+    validates :birthday
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
-    validates :first_name
-    validates :last_name
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/} do
-    validates :first_name_kana
-    validates :last_name_kana
-  end
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
   
 end
