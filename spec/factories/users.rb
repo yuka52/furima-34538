5f95b53bc2b4b1953a8ac66f7d1c2a6501db26d4
yuka52 <clrwtr.710@gmail.com>
2021-03-04 16:37:53 +0900

    テストコード修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.name }
     email    { Faker::Internet.email }
-    password = Faker::Internet.password(min_length: 6)
+    password = '1a' + Faker::Internet.password(min_length: 6, max_length: 128)
     password { password }
     password_confirmation { password }
     last_name { '山田' }
