3727076b289e1a73e58dfeab2429879806494a6a
yuka52 <clrwtr.710@gmail.com>
2021-03-06 18:05:52 +0900

    エラーメッセージの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,19 +12,21 @@ class Item < ApplicationRecord
    validates :description
    validates :image
 
-   with_options numericality: {oter_than: 1 } do 
-      validates :category_id
+   validates :price, numericality: { only_integer: true,
+   greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999,
+   message: 'Out of setting range'
+   },
+   format: { with: /\A[0-9]+\z/, message: "Half-width number" }
+
+   with_options numericality: {other_than: 1, message: 'Select' } do 
+      validates :category_id 
       validates :condition_id 
       validates :delivery_fee_id 
       validates :prefecture_id  
       validates :days_to_ship_id
    end
    
-   with_options numericality: { only_integer: true,
-     greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999
-     },
-     format: {with: /\A[0-9]+\z/} do
-      validates :price
-    end
+    
+      
   end 
 end
