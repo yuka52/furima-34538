37cc74a4e21d068d7fdf4927ed1f7db48baed969
yuka52 <clrwtr.710@gmail.com>
2021-03-07 16:29:55 +0900

    itemモデルの単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,19 +4,14 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :delivery_fee
   belongs_to :Prefecture
-  belongs_to :day_to_ship
+  belongs_to :days_to_ship
+  belongs_to :user
   has_one_attached :image
   
   with_options presence: true do
    validates :name  
    validates :description
    validates :image
-
-   validates :price, numericality: { only_integer: true,
-   greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999,
-   message: 'Out of setting range'
-   },
-   format: { with: /\A[0-9]+\z/, message: "Half-width number" }
 
    with_options numericality: {other_than: 1, message: 'Select' } do 
       validates :category_id 
@@ -26,7 +21,11 @@ class Item < ApplicationRecord
       validates :days_to_ship_id
    end
    
-    
+     validates :price, numericality: { only_integer: true,
+     greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999,
+     message: 'Out of setting range'
+     }
+     validates :price, numericality: { with: /\A[0-9]+\z/, message: "Half-width number" }
       
   end 
 end
