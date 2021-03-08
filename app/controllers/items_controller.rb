923cf9d502089d1c99e77dec366c65c61f9a16a5
yuka52 <clrwtr.710@gmail.com>
2021-03-08 18:57:44 +0900

    updateアクションを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,17 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+     redirect_to item_path(item)
+    else
+     @item = item
+     render :edit
+    end
   end
 
   private
