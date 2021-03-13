19d3d05a7976b001c5dab0da900ff357dc721ab5
yuka52 <clrwtr.710@gmail.com>
2021-03-13 13:18:32 +0900

    テストコード訂正

--- spec/models/order_destination_spec.rb
+++ spec/models/order_destination_spec.rb
@@ -9,68 +9,71 @@ RSpec.describe OrderDestination, type: :model do
       sleep 0.1
     end
 
-    it 'すべての値が正しく入力されていれば配送先の情報が保存できること' do
-      expect(@order_destination).to be_valid
+    context '配送先住所が保存できる場合' do
+      it 'すべての値が正しく入力されていれば配送先の情報が保存できること' do
+        expect(@order_destination).to be_valid
+      end
+
+      it 'buildingは空でも保存できること' do
+        @order_destination.building = ''
+        expect(@order_destination).to be_valid
+      end
     end
 
-    it 'buildingは空でも保存できること' do
-      @order_destination.building = ''
-      expect(@order_destination).to be_valid
-    end
+    context '配送先住所が保存できない場合' do
+      it 'postal_codeが空だと保存できないこと' do
+        @order_destination.postal_code = ''
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Postal code can't be blank")
+      end
 
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @order_destination.postal_code = '1234567'
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Postal code Input correctly")
+      end
 
-    it 'postal_codeが空だと保存できないこと' do
-      @order_destination.postal_code = ''
-      @order_destination.valid?
-      expect(@order_destination.errors.full_messages).to include("Postal code can't be blank")
-    end
+      it 'postal_codeが全角だと保存できないこと' do
+        @order_destination.postal_code = '１２３-４５６７'
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Postal code Input correctly")
+      end
 
-    it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
-      @order_destination.postal_code = '1234567'
-      @order_destination.valid?
-      expect(@order_destination.errors.full_messages).to include("Postal code Input correctly")
-    end
+      it 'prefectureを選択していないと保存できないこと' do
+        @order_destination.prefecture_id = ''
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Prefecture can't be blank")
+      end
 
-    it 'postal_codeが全角だと保存できないこと' do
-      @order_destination.postal_code = '１２３-４５６７'
-      @order_destination.valid?
-      expect(@order_destination.errors.full_messages).to include("Postal code Input correctly")
-    end
+      it 'prefectureは１を選択すると保存できないこと' do
+        @order_destination.prefecture_id = 1
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Prefecture must be other than 1")
+      end
 
-    it 'prefectureを選択していないと保存できないこと' do
-      @order_destination.prefecture_id = ''
-      @order_destination.valid?
-      expect(@order_destination.errors.full_messages).to include("Prefecture can't be blank")
-    end
+      it 'municipalityが空だと保存できないこと' do
+        @order_destination.municipality = ''
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Municipality can't be blank")
+      end
 
-    it 'prefectureは１を選択すると保存できないこと' do
-      @order_destination.prefecture_id = 1
-      @order_destination.valid?
-      expect(@order_destination.errors.full_messages).to include("Prefecture must be other than 1")
-    end
+      it 'addressが空だと保存できないこと' do
+        @order_destination.address = ''
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Address can't be blank")
+      end
+      
+      it 'phone_numberが空だと保存できないこと' do
+        @order_destination.phone_number = ''
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Phone number can't be blank")
+      end
 
-    it 'municipalityが空だと保存できないこと' do
-      @order_destination.municipality = ''
-      @order_destination.valid?
-      expect(@order_destination.errors.full_messages).to include("Municipality can't be blank")
-    end
-
-    it 'addressが空だと保存できないこと' do
-      @order_destination.address = ''
-      @order_destination.valid?
-      expect(@order_destination.errors.full_messages).to include("Address can't be blank")
-    end
-    
-    it 'phone_numberが空だと保存できないこと' do
-      @order_destination.phone_number = ''
-      @order_destination.valid?
-      expect(@order_destination.errors.full_messages).to include("Phone number can't be blank")
-    end
-
-    it "tokenが空だと保存できないこと" do
-      @order_destination.token = nil
-      @order_destination.valid?
-      expect(@order_destination.errors.full_messages).to include("Token can't be blank")
-    end
+      it "tokenが空だと保存できないこと" do
+        @order_destination.token = nil
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Token can't be blank")
+      end
+    end 
   end
 end
