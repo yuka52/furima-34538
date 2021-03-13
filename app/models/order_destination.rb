e3ccfbdbb1096128551c7c97844867d6f5ed01d7
yuka52 <clrwtr.710@gmail.com>
2021-03-13 18:12:32 +0900

    attr_accessorのorder_idを削除

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,6 +1,6 @@
 class OrderDestination
   include ActiveModel::Model
-  attr_accessor   :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :order_id, :token
+  attr_accessor   :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :token
   
   with_options presence: true do
     validates :user_id
